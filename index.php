627cec848cf5bd804813c94edcd9b891942e49ea
Boy4ik-2k22 <aroneger2018@gmail.com>
2022-04-05 21:54:33 +0300

    setup minimum stability of project

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use Blog\PostMapper;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
+$config = include 'config/database.php';
+$dsn = $config['dsn'];
+$username = $config['username'];
+$password = $config['password'];
+
+try {
+    $connection = new PDO($dsn, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $exception) {
+    echo "Database error: " . $exception->getMessage();
+    exit;
+}
+
+$postMapper = new PostMapper($connection);
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) use ($view, $postMapper){
+    $posts = $postMapper->getList('ASC');
+
+    $body = $view->render('index.twig', [
+        'posts' => $posts
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/about', function (Request $request, Response $response, $args) use ($view){
+    $body = $view->render('about.twig', [
+        "name" => "Andriy"
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $postMapper){
+    $post = $postMapper->getByUrlKey((string) $args['url_key']);
+
+    if (empty($post)) {
+        $body = $view->render('not-found.twig');
+    } else {
+        $body = $view->render('post.twig', [
+            "post" => $post
+        ]);
+    }
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->run();
