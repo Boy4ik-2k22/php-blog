0a06025f2c3f68da3280b62817543850bba7a7f3
Boy4ik-2k22 <aroneger2018@gmail.com>
2022-04-30 18:31:36 +0300

    add pagination into blog page

--- src/PostMapper.php
+++ src/PostMapper.php
@@ -57,4 +57,16 @@ class PostMapper
 
         return $statement->fetchAll();
     }
+
+    /**
+     * @return int
+     */
+    public function getTotalCount(): int
+    {
+        $statement = $this->connection->prepare('SELECT count(post_id) as total FROM post');
+
+        $statement->execute();
+
+        return (int) ($statement->fetchColumn() ?? 0);
+    }
 }
