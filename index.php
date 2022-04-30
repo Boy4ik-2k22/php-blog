107847a0883651a1caa9d2ce5669598c87f8611b
Boy4ik-2k22 <aroneger2018@gmail.com>
2022-04-30 22:44:16 +0300

    create PDO provider

--- index.php
+++ index.php
@@ -1,84 +1,81 @@
 <?php
 
+use Blog\Database;
+use Blog\LatestPosts;
+use Blog\Slim\TwigMiddleware;
+use DevCoder\DotEnv;
 use DI\ContainerBuilder;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 use Blog\PostMapper;
-use Blog\LatestPosts;
-use Blog\Slim\TwigMiddleware;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $builder = new ContainerBuilder();
 $builder->addDefinitions('config/di.php');
+(new DotEnv(__DIR__ . '/.env'))->load();
 
 $container = $builder->build();
 
 AppFactory::setContainer($container);
 
-$config = include 'config/database.php';
-$dsn = $config['dsn'];
-$username = $config['username'];
-$password = $config['password'];
-
-try {
-    $connection = new PDO($dsn, $username, $password);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (PDOException $exception) {
-    echo "Database error: " . $exception->getMessage();
-    exit;
-}
-
+// Create app
 $app = AppFactory::create();
 
 $view = $container->get(Environment::class);
-
 $app->add(new TwigMiddleware($view));
 
-$app->get('/', function (Request $request, Response $response) use ($view, $connection){
+$connection = $container->get(Database::class)->getConnection();
+
+$app->get('/', function (Request $request, Response $response) use ($view, $connection) {
     $latestPosts = new LatestPosts($connection);
     $posts = $latestPosts->get(3);
 
     $body = $view->render('index.twig', [
         'posts' => $posts
     ]);
+
     $response->getBody()->write($body);
+
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response) use ($view){
+$app->get('/about', function (Request $request, Response $response) use ($view) {
     $body = $view->render('about.twig', [
-        "name" => "Andriy"
+        'name' => 'Max'
     ]);
+
     $response->getBody()->write($body);
+
     return $response;
 });
 
-$app->get('/blog[/{page}]', function (Request $request, Response $response, $args) use ($view, $connection){
+$app->get('/blog[/{page}]', function (Request $request, Response $response, $args) use ($view, $connection) {
     $postMapper = new PostMapper($connection);
 
     $page = isset($args['page']) ? (int) $args['page'] : 1;
-    $totalCount = $postMapper->getTotalCount();
     $limit = 2;
 
     $posts = $postMapper->getList($page, $limit, 'DESC');
 
+    $totalCount = $postMapper->getTotalCount();
+
     $body = $view->render('blog.twig', [
         'posts' => $posts,
         'pagination' => [
-            'current' => $page,
-            'paging' => ceil($totalCount / $limit)
+        'current' => $page,
+        'paging' => ceil($totalCount / $limit),
         ]
     ]);
+
     $response->getBody()->write($body);
+
     return $response;
 });
 
-$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $connection){
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $connection) {
     $postMapper = new PostMapper($connection);
 
     $post = $postMapper->getByUrlKey((string) $args['url_key']);
@@ -87,10 +84,12 @@ $app->get('/{url_key}', function (Request $request, Response $response, $args) u
         $body = $view->render('not-found.twig');
     } else {
         $body = $view->render('post.twig', [
-            "post" => $post
+        'post' => $post
         ]);
     }
+
     $response->getBody()->write($body);
+
     return $response;
 });
 
